4911f63d7da22467573154f3ec25916432b1b6c3
citizen0x00 <83061311+citizen0x00@users.noreply.github.com>
2023-11-02 17:43:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import os
 import argparse
 
-#####################CONFIGMODE###########################
-opnsense = "10.0.1.1" #IP to the OPNSense on the network
-key = "" #Key made in the usersection of OPNSense
-secret = #Secretkey made in the usersection of OPNSense
-alias = "" #The alias you want to add the IP to | Comment out this if you want to parse the alias as an argument
-##########################################################
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-ip","--ip", help="IP to add to OPNSense")
 argParser.add_argument("-a","--alias", help="Alias you want to add the IP to")
 args = argParser.parse_args()
 ip = args.ip
+#####################CONFIGMODE###########################
+opnsense = "10.0.1.1" #IP to the OPNSense on the network
+key = "" #Key made in the usersection of OPNSense
+secret = #Secretkey made in the usersection of OPNSense
+alias = "" #The alias you want to add the IP to | Comment out this if you want to parse the alias as an argument
 #alias = args.alias #uncomment out this if you want alias to hardcode the alias
+##########################################################
 os.system('curl --header "Content-Type: application/json" --basic --user "'+key+'":"'+secret+'" --insecure --verbose --data \'{"address":"'+ip+'"}\' http://'+opnsense+'/api/firewall/alias_util/add/'+alias+'') #Adding IP to alias
 os.system('curl -X POST -d "" -k --user "'+key+'":"'+secret+'" http://'+opnsense+'/api/firewall/alias/reconfigure') #"Save/Commiting" the changes
-
 print("\nDone - "+ip+" is added to the alias: "+alias+"!")
